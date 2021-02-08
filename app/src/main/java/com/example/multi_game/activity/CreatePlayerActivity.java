8b579db9ad124ae59d07db8e3488817616370fd3
ArnaudSau <arnaud.saudrais@yahoo.com>
2021-02-08 14:39:23 +0100

    Add player bdd

--- app/src/main/java/com/example/multi_game/activity/CreatePlayerActivity.java
+++ app/src/main/java/com/example/multi_game/activity/CreatePlayerActivity.java
@@ -9,77 +9,119 @@ import androidx.databinding.DataBindingUtil;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.multi_game.R;
+import com.example.multi_game.dao.AppDatabase;
 import com.example.multi_game.databinding.ActivityCreatePlayerBinding;
+import com.example.multi_game.model.Player;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.squareup.picasso.Picasso;
 
 public class CreatePlayerActivity extends AppCompatActivity {
-
-    private static final int REQUEST_IMAGE = 1001;
-
-    private static final int REQUEST_LOCALISATION_PERMISION = 2001;
-
+    private static final int REQUEST_IMAGE = 7;
+    private static final int REQUEST_LOCALISATION_PERMISSION = 2001;
     private ActivityCreatePlayerBinding binding;
-
-    private String imagePath;
-
+    private String pictureUrl;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_create_player);
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_player);
         binding.createPlayerImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setDataAndType(MediaStore.Images.Media.INTERNAL_CONTENT_URI, "image/*");
-                startActivityForResult(Intent.createChooser(intent,"Choix de la photo"), REQUEST_IMAGE);
-                //CreatePlayerActivity.this.startActivityForResult(intent,REQUEST_IMAGE);
+                //startActivityForResult(Intent.createChooser(intent,"Choix de la photo"), REQUEST_IMAGE);
+                startActivityForResult(intent, REQUEST_IMAGE);
             }
         });
-
         binding.createPlayerCompas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ActivityCompat.checkSelfPermission(CreatePlayerActivity.this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(CreatePlayerActivity.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-                    ActivityCompat.requestPermissions(CreatePlayerActivity.this,new String[]{
+                if (checkLocationAuthorized()) {
+                    getUserLocation();
+                } else {
+                    ActivityCompat.requestPermissions(CreatePlayerActivity.this, new String[]{
                             Manifest.permission.ACCESS_COARSE_LOCATION,
                             Manifest.permission.ACCESS_FINE_LOCATION
-                    }, REQUEST_LOCALISATION_PERMISION);
+                    }, REQUEST_LOCALISATION_PERMISSION);
                 }
             }
         });
+
+        binding.createPlayerValidate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!binding.createPlayerNom.getText().toString().isEmpty()
+                        && !binding.createPlayerPrenom.getText().toString().isEmpty()
+                        && !binding.createPlayerLocalisation.getText().toString().isEmpty()
+                        && !binding.createPlayerAge.getText().toString().isEmpty()
+                        && pictureUrl != null)
+                {
+                    Player player = new Player(binding.createPlayerNom.getText().toString(),
+                            binding.createPlayerPrenom.getText().toString(),
+                            Integer.parseInt(binding.createPlayerAge.getText().toString()),
+                            binding.createPlayerLocalisation.getText().toString(),
+                            pictureUrl);
+
+                    AppDatabase.getDatabase(getApplicationContext()).appDao().insert(player);
+                }else{
+                    Toast.makeText(CreatePlayerActivity.this,"Informations manquantes",Toast.LENGTH_LONG).show();
+                }
+                //Player player = new Player("nom","prenom",Integer.parseInt(binding.createPlayerAge.getText().toString()),"loc","picture");
+            }
+        });
+
+
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_LOCALISATION_PERMISION &&
-                ActivityCompat.checkSelfPermission(CreatePlayerActivity.this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(CreatePlayerActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (requestCode == REQUEST_LOCALISATION_PERMISSION && checkLocationAuthorized()) {
             getUserLocation();
         }
     }
-
-    public void getUserLocation(){
-
+    private void getUserLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.getFusedLocationProviderClient(this).getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            binding.createPlayerLocalisation.setText(getString(R.string.location_lat_lng,
+                                    location.getLatitude(), location.getLongitude()));
+                        }
+                    }
+                });
     }
 
+    private boolean checkLocationAuthorized(){
+        return ActivityCompat.checkSelfPermission(CreatePlayerActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(CreatePlayerActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == REQUEST_IMAGE && resultCode == RESULT_OK){
-            imagePath = data.getDataString();
-            Picasso.get().load(data.getData()).into(binding.createPlayerImage);
+        if (requestCode == REQUEST_IMAGE && resultCode == RESULT_OK) {
+            Picasso.get().load(data.getData()).centerCrop().fit().into(binding.createPlayerImage);
+            pictureUrl = data.getDataString();
         }
     }
 }
